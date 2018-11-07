e6174e566192e19a0c74b13d85ad1350be429ed9
Mina <minap@equitise.com>
2018-11-07 17:25:02 +1100

    add a tag for external link in header

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -14,14 +14,20 @@
         <div :class="['navbar-menu',{'is-active': display}]" id="navbarMenu">
           <div class="navbar-end">
             <span class="navbar-item" v-for="(item, key) in navbar" :key="key">
-              <router-link class="navbar-item button is-white is-outlined" :to="{path:item.router}">
+              <router-link v-if="key <= 3" class="navbar-item button is-white is-outlined" :to="{path:item.router}">
                   <span class="icon">
-                      <!-- <i :class="item.fa +  ' fa-' +  item.icon" ></i> -->
                       <font-awesome-icon v-if="key == 6" :icon="['fab', item.icon]" />
                       <font-awesome-icon v-else :icon="['fas', item.icon]" />
                   </span>
                   <span v-text="item.name"></span>
               </router-link>
+              <a v-if="key > 3"  class="navbar-item button is-white is-outlined" :href="item.link">
+                <span class="icon">
+                    <font-awesome-icon v-if="key == 6" :icon="['fab', item.icon]" />
+                    <font-awesome-icon v-else :icon="['fas', item.icon]" />
+                </span>
+                <span v-text="item.name"></span>
+              </a>
             </span>
           </div>
         </div>
@@ -37,9 +43,9 @@ export default {
       {name: '2', origin: 'Projects', router: '/projects', icon: 'project-diagram'},
       {name: '3', origin: 'About', router: '/about', icon: 'info'},
       {name: '4', origin: 'Skills', router: '/skills', icon: 'star'},
-      {name: '5', origin: 'Resume', router: 'https://drive.google.com/file/d/1WJG6qCVdnLnF2eu-s02A4Cd3hto6Fp92/view?usp=sharing', icon: 'file-alt'},
-      {name: '6', origin: 'Contacts', router: 'https://github.com/Minaparnian', icon: 'envelope'},
-      {name: '7', origin: 'Github', router: '/https://github.com/Minaparnian', icon: 'github-alt'}
+      {name: '5', origin: 'Resume', link: 'https://drive.google.com/file/d/1WJG6qCVdnLnF2eu-s02A4Cd3hto6Fp92/view?usp=sharing', icon: 'file-alt'},
+      {name: '6', origin: 'Contacts', link: 'https://github.com/Minaparnian', icon: 'envelope'},
+      {name: '7', origin: 'Github', link: 'https://github.com/Minaparnian', icon: 'github-alt'}
     ],
     display: false
   })
